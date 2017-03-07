ecbe53a713c08d8b2692637df7131aa13d5017de
krishnapriya <krishnapriyakamesh@gmail.com>
2017-03-07 15:31:37 -0600

    step 5 in exercise 11 done

--- main.js
+++ main.js
@@ -6,4 +6,10 @@ const db = new sqlite3.Database('./musichistory.db',(err)=>{console.log("connect
 // console.log(db)
 //
 // db.run('INSERT INTO Artist(ArtistName,YearEstablished) VALUES ("D.Imman",1990)')
-db.run('INSERT INTO Album(Title,ReleaseDate,AlbumLength,Label,ArtistId,GenreId) VALUES ("Jilla2","12/21/2013",34,"Vijay Movie",(SELECT ArtistId FROM Artist WHERE ArtistName="D.Imman"),(SELECT GenreId FROM Genre WHERE Label="Jazz"))',(err)=>{console.log(err)});
+// db.run('INSERT INTO Album(Title,ReleaseDate,AlbumLength,Label,ArtistId,GenreId) VALUES ("Saattai","08/12/2012",58,"Saattai Movie",(SELECT ArtistId FROM Artist WHERE ArtistName="D.Imman"),(SELECT GenreId FROM Genre WHERE Label="Country"))',(err)=>{console.log(err)});
+// db.run('INSERT INTO Song(Title,SongLength,ReleaseDate,GenreId,ArtistId,AlbumId) VALUES ("Pattu onnu",5,"12/21/2013",(SELECT GenreId FROM Genre WHERE Label="Jazz"),(SELECT ArtistId FROM Artist WHERE ArtistName ="D.Imman"),(SELECT AlbumId FROM Album WHERE Title = "Jilla2"))')
+db.all(`SELECT Song.Title,Album.Title,Artist.ArtistName FROM Album
+  LEFT JOIN Song ON Song.AlbumId = Album.AlbumId
+  LEFT JOIN Artist ON Artist.ArtistId = Album.ArtistId`,(err,allRows)=>{
+  console.log({allRows})
+});
